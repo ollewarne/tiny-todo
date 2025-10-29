182d3d4fbe4c46b48982217085fb0b48fd9b2d19
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-10-29 23:08:46 +0100

    update script to use text input and keydown events

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 const list = document.getElementById("todo-list");
 const addButton = document.getElementById("add-item");
+const input = document.getElementById("text-input");
+const showAll = document.getElementById("button-all");
+const showDone = document.getElementById("button-done");
 
 
 addButton.addEventListener("click", () => {
-    let number = Math.floor(Math.random() * 1000)
     const item = document.createElement("li")
-    item.textContent = "item number: " + number
-    list.appendChild(item);
+    if (input.value.trim() !== "") {
+        item.textContent = input.value
+        list.appendChild(item);
+        input.value = "";
+    }
+})
+
+input.addEventListener('keydown', (event) => {
+    if (event.key === "Enter" && input.value.trim() !== "") {
+        let item = document.createElement("li")
+        item.textContent = input.value
+        list.appendChild(item);
+        input.value = "";
+    }
 })
 
 list.addEventListener("click", (event) => {
     if (event.target.tagName === "LI") {
-        list.removeChild(event.target);
+        event.target.classList.toggle("done");
+    }
+})
+
+showDone.addEventListener('click', () => {
+    for (item of list.children) {
+        if (!item.classList.contains("done")) {
+            item.classList.add("hidden");
+        }
+    }
+})
+
+showAll.addEventListener('click', () => {
+    for (item of list.children) {
+        item.classList.remove("hidden")
     }
 })
